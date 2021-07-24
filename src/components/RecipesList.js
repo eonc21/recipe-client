a836e307fdc26df874b3cd234f107a7cb0e862d0
Elinor <e.onc@student.tudelft.nl>
2021-07-24 20:45:06 +0200

    Move recipe fetch to the wrapper class and passed as prop to the recipe list

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -2,17 +2,8 @@ import { useEffect, useState } from "react";
 import Recipe from "./Recipe";
 import styles from "../styling/RecipesList.module.css"
 
-const RecipesList = () => {
-    const [recipes, setRecipes] = useState([])
+const RecipesList = ({ recipes }) => {
 
-    useEffect(() => {
-        fetch("http://localhost:5001/api/recipes/getAll")
-        .then(response => response.json())
-        .then(res =>{
-             console.log(res)  
-             setRecipes(res)
-            })
-    }, [])
     return ( 
         <div className={styles.container}>
             {recipes.map(recipe => (
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -2,9 +2,28 @@ import CategoriesList from "./CategoriesList";
 import RecipesList from "./RecipesList";
 import RecipeMessage from "./RecipeMessage";
 import styles from '../styling/Wrapper.module.css'
+import { useEffect, useState } from "react";
 
 
 const Wrapper = () => {
+    
+    const [recipes, setRecipes] = useState([])
+    const [categories, setCategories] = useState([])
+
+    /**
+     * useEffect that fetches the recipes
+     * from the getAll endpoint.
+     */
+
+    useEffect(() => {
+        fetch("http://localhost:5001/api/recipes/getAll")
+        .then(response => response.json())
+        .then(res =>{
+             console.log(res)  
+             setRecipes(res)
+            })
+    }, [])
+
     return ( 
         <div className={styles.container}>
             <div className={styles.row}>
@@ -13,7 +32,8 @@ const Wrapper = () => {
             </div>
             
             <div className={styles.recipeContainer}>
-                <RecipesList />
+                <RecipesList 
+                    recipes={recipes} />
 
             </div>
         </div>
