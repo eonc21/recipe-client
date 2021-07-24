c09275e099fdbc6ab3fbd294c72fc9c221c3d630
Elinor <e.onc@student.tudelft.nl>
2021-07-24 12:17:15 +0200

    Add border radius on the right of the categories bar

--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -6,24 +6,40 @@ const CategoriesList = () => {
 
     const onClick = (index) => {
 
-        if (colour[index] === 'orange') {
-            let temp = [...colour]
-            temp[index] = "white"
+        let temp = [...colour]
+        let tempFont = [...fontColour]
+        let borderRadius = [...border]
+
+        if (colour[index] === '#ffc53b') {
+            temp[index] = "transparent"
+            tempFont[index] = "black"
             setColour(temp)
+            setFontColour(tempFont)
         
         } else {
-            let temp = [...colour]
-            temp[index] = "orange"
+            temp[index] = "#ffc53b"
+            tempFont[index] = "white"
             setColour(temp)
+            setFontColour(tempFont)
+
+            if (index === 4) {
+                borderRadius[index] = "10px"
+                setBorder(borderRadius)
+                console.log(index, lastElement)
+
+
+            }
         }
         
         // console.log(colour)   this went right, it's good
-
         
     }
+    const [colour, setColour] = useState([5])
+    const [fontColour, setFontColour] = useState([5])
+    const lastElement = colour.length
 
-    const [colour, setColour] = useState([])
     const [categories, setCategories] = useState([])
+    const [border, setBorder] = useState([5])
 
     useEffect(() => {
         fetch("http://localhost:5001/api/categories/getAll")
@@ -40,7 +56,9 @@ const CategoriesList = () => {
             <div className={styles.independentContainer}>All</div>
             {categories.map((category, index) => (
                 <Category 
+                border={border[index]}
                 colour={colour[index]}
+                fontColour={fontColour[index]}
                 categoryTitle={category.stringName}
                 onClick={() => { 
                     onClick(index)
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,11 +1,14 @@
 import styles from '../styling/Category.module.css'
 
 
-const Category = ({ categoryTitle, onClick, colour }) => {
+const Category = ({ categoryTitle, onClick, colour, fontColour, border }) => {
     return ( 
         <div
          className={styles.container}
-         style={{backgroundColor: colour}}
+         style={{backgroundColor: colour,
+        color: fontColour,
+        borderBottomRightRadius: border,
+        borderTopRightRadius: border }}
 
          onClick={onClick}>
             {categoryTitle}
